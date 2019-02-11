ec05d1e9c83e263a53460de9da09143b74430c5f
maxhfung <maxhfung@gmail.com>
2019-02-10 16:37:09 -0800

    ptank units

--- F/P_tank/P_TANK_MAIN.py
+++ F/P_tank/P_TANK_MAIN.py
@@ -6,10 +6,10 @@ from P_tank import P_tank
 
 
 oxidizer_fuel_ratio = 3.21
-propellant_mass = 34.87
+propellant_mass = 161 #kg
 l_d_ratio = 25.23
-interval_check_precision = .34
+
 
 ptank = P_tank()
-tup = ptank.tank_length_diam(oxidizer_fuel_ratio,propellant_mass, l_d_ratio, interval_check_precision)
+tup = ptank.tank_length_diam(oxidizer_fuel_ratio,propellant_mass, l_d_ratio)
 print(tup[0],tup[1])
--- F/P_tank/P_tank.py
+++ F/P_tank/P_tank.py
@@ -3,16 +3,50 @@ import numpy as np
 
 #Create a Rocket Class
 class P_tank():
+    '''
+    Determine tank length from type, diameter, mass, and ulage.
 
-    lox_density = 1.140
-    kero_density = 1.02
+    Parameters
+    ----------
+    type : string
+        Tank type (either propellant or lox)
+    diameter : numeric
+        Diameter of the rocket body cross section
+    x_mass : numeric
+        Total mass of propellant / lox within tank
+    ulage : numeric
+        Total mass of ulage within tank
 
+    Returns
+    -------
+    tank_length_diam : numeric
+        Length of tank
+
+    Notes
+    -----
+    The following assumptions are made
+
+    ==========================  ============================
+    Parameter                   Value
+    ==========================  ============================
+    Density of RP-1             804.59 kg/m^3 @ 1 atm   [1]
+    Density of Liquid Oxygen    1141 kg/m^3 @ 293 K     [2]
+    ==========================  ============================
+
+    Created by Buddha and Max for Loyola MARS Base11 1-30-2019
+
+    References
+    ----------
+    [1]
+    [2] https://ws680.nist.gov/publication/get_pdf.cfm?pub_id=832303
+    '''
 
     def __init__(self):
-        p = self
+        self.lox_density = 1141     #kg/m**3 [1]
+        self.kero_density = 804.59   #kg/m**3 [2]
 
     def calculate_tank_volume(self, oxidizer_fuel_ratio, propellant_mass):
-        kero_mass = propellant_mass/(1+oxidizer_fuel_ratio)
+        kero_mass = propellant_mass/(1 + oxidizer_fuel_ratio)
         lox_mass = propellant_mass - kero_mass
 
         kero_vol = kero_mass * self.kero_density
@@ -21,7 +55,7 @@ class P_tank():
         return kero_vol + lox_vol
 
 
-    def tank_length_diam_sub(self,volume,l_d_ratio, interval_check_precision):
+    def tank_length_diam_sub(self,volume,l_d_ratio):
 
         diameter = ((4*volume)/(math.pi*l_d_ratio))**(1. /3)
         length = l_d_ratio*diameter
@@ -29,6 +63,6 @@ class P_tank():
         return diameter, length, l_d_ratio
 
 
-    def tank_length_diam(self, oxidizer_fuel_ratio, propellant_mass,l_d_ratio, interval_check_precision):
+    def tank_length_diam(self, oxidizer_fuel_ratio, propellant_mass,l_d_ratio):
         vol = self.calculate_tank_volume(oxidizer_fuel_ratio, propellant_mass)
-        return self.tank_length_diam_sub(vol,l_d_ratio, interval_check_precision)
+        return self.tank_length_diam_sub(vol,l_d_ratio)
